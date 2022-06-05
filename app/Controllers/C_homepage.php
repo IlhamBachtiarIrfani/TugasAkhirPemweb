10df331a8a9d4be0be018e662907ac2ad86160e6
Japeran <achmadzhafran@student.ub.ac.id>
2022-06-05 13:02:11 +0700

    progress eug ni boss

--- app/Controllers/C_homepage.php
+++ app/Controllers/C_homepage.php
@@ -7,16 +7,7 @@ use App\Models\M_User;
 class C_homepage  extends BaseController
 {
     public function index()
-<<<<<<< HEAD
     {    
         return view("auth/homepage");
-=======
-    {
-        $ModelUser = new M_User();
-
-        $data["user"] = $ModelUser->getAllUser();
-        
-        return view('homepage', $data);
->>>>>>> d0caa8795be806e7bc4bc29300df10f0993eefae
     }
 }
