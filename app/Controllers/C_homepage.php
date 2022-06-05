4743b1ae0e5d9a9c3cd310e8453a48bc4dbf6dbd
Asyam Fadhilah Akbar <asyam.akbar@yahoo.com>
2022-06-05 13:19:52 +0700

    Update C_homepage.php

--- app/Controllers/C_homepage.php
+++ app/Controllers/C_homepage.php
@@ -7,16 +7,11 @@ use App\Models\M_User;
 class C_homepage  extends BaseController
 {
     public function index()
-<<<<<<< HEAD
     {
         $ModelUser = new M_User();
 
         $data["user"] = $ModelUser->getAllUser();
         
         return view('homepage', $data);
-=======
-    {    
-        return view("auth/homepage");
->>>>>>> 10df331a8a9d4be0be018e662907ac2ad86160e6
     }
 }
