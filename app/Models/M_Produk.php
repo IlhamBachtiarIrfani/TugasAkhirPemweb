9e1c4b9cad5788990164f09bc9fdd4df1405c76b
Japeran <achmadzhafran@student.ub.ac.id>
2022-06-19 12:32:25 +0700

    bikin model produk

--- app/Models/M_Produk.php
+++ app/Models/M_Produk.php
@@ -8,7 +8,7 @@ class M_Produk extends Model {
 
      public function addProduk($namaProduk, $foto, $deskripsi, $harga, $rating)
      {
-         $query = $this->db->table("slider");
+         $query = $this->db->table("produk mitra");
  
          $data = [
              "NamaProduk" => $namaProduk,
@@ -22,4 +22,72 @@ class M_Produk extends Model {
  
          return $this->db->affectedRows();
      }
+
+     public function getAllSlider()
+    {
+        // ini buat inisiasi tabel di db
+        $query = $this->db->table("produk mitra");
+
+        // ini buat select, kasih paramter sesuai kolom yang mau diambil
+        $query->select("*");
+
+        // ini buat ngeeksekusi querynya terus ambil respon nya
+        return $query->get()->getResultArray();
+    }
+
+    // ===== INI METHOD BUAT AMBIL SEMUA DATA =====
+    public function getSliderById($id)
+    {
+        // ini buat inisiasi tabel di db
+        $query = $this->db->table("produk mitra");
+
+        // ini buat select, kasih paramter sesuai kolom yang mau diambil
+        $query->select("*");
+
+        // ini buat query where biar id_slider = $id
+        $query->where("idProduk", $id);
+
+        // ini buat ngeeksekusi querynya terus ambil respon nya
+        // get row biar ambil 1 data doang
+        return $query->get()->getRowArray();
+    }
+
+     // ===== INI METHOD BUAT UPDATE DATA =====
+     public function editSlider($id, $namaProduk, $foto, $deskripsi, $harga, $rating)
+     {
+         $query = $this->db->table("produk mitra");
+ 
+         $data = [
+            "NamaProduk" => $namaProduk,
+            "DeskripsiProduk" => $deskripsi,
+            "FotoProduk" => $foto,
+            "harga" => $harga,
+            "rating" => $rating,
+         ];
+ 
+         $query->set($data);
+ 
+         // ini buat query where biar id_slider = $id
+         $query->where("idProduk", $id);
+ 
+         // ini buat ngeeksekusi query updatenya terus ambil respon nya
+         $query->update();
+ 
+         // ini buat cari tau ada berapa data yang kena efek sama query
+         return $this->db->affectedRows();
+     }
+
+     public function deleteSlider($id)
+    {
+        $query = $this->db->table("produk mitra");
+
+        // ini buat query where biar id_slider = $id
+        $query->where("idProduk", $id);
+
+        // ini buat ngeeksekusi query deletenya terus ambil respon nya
+        $query->delete();
+
+        // ini buat cari tau ada berapa data yang kena efek sama query
+        return $this->db->affectedRows();
+    }
 }
--- app/Views/general/header.php
+++ app/Views/general/header.php
@@ -21,15 +21,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<<<<<<< HEAD
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script></head>
-=======
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css-font-awesome.min.css" /> 
     <link rel="stylesheet" href="assets/owl.carousel.min.css" />
     <link rel="stylesheet" href="assets/owl.theme.default.min.css" />
 </head>
->>>>>>> 10d9e53631855ea83f2e543feaba9aeb7eb02abc
 
 <body style="width: 100%; overflow-x: hidden;">
     <!-- HEADER -->
