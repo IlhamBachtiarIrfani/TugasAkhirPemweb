a825d1951c426757bff40c2c72abf3b2016d2852
Raflymumtz <raflymumtaz23@gmail.com>
2022-06-20 23:44:27 +0700

    Tambahan

--- app/Views/general/header.php
+++ app/Views/general/header.php
@@ -65,13 +65,13 @@
                     <a href="<?= base_url("C_Webinar") ?>" class="nav-link">Webinar</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= base_url("C_ProdukAuth") ?>" class="nav-link">Produk</a>
+                    <a href="<?= base_url("C_Produk") ?>" class="nav-link">Produk</a>
                 </li>
                 <li class="nav-item">
                     <a href="<?= base_url() ?>" class="nav-link ">Mitra</a>
                 </li>
                 <li class="nav-item">
-                    <a href="<?= base_url() ?>" class="nav-link">Tentang Kami</a>
+                    <a href="<?= base_url("C_TentangKami") ?>" class="nav-link">Tentang Kami</a>
                 </li>
                 <?php if (isset($userData)) { ?>
                     <li class="nav-item">
